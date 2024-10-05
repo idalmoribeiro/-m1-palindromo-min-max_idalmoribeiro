6e386f69bdb36fe15e44f4fd8611245a3f814560
idalmoribeiro <idalmoribeiro6@hotmail.com>
2024-10-05 15:43:08 -0300

    tentativa2

--- script.js
+++ script.js
@@ -1,35 +1,36 @@
 function isPalindrome(texto){
 
-        let upControl = [];
-        let downControl = [];
-        let saida1 = "";
-        let saida2 = "";
-        let startReverse = texto.length -1;
-        let texto2 = texto.toLowerCase();
-        let saida3 = "";
-        let saida4 = "";
-       
-    
-        for (let i=0; i<= (texto2.length -1); i++){
-            upControl.push(texto2[i]);
+    let upControl = [];
+    let downControl = [];
+    let saida1 = "";
+    let saida2 = "";
+    let startReverse = texto.length -1;
+    let texto2 = texto.toLowerCase();
+    let saida3 = "";
+    let saida4 = "";
+   
+
+    for (let i=0; i<= (texto2.length -1); i++){
+        upControl.push(texto2[i]);
+    }
+        for (let j = startReverse; j>=0; j--){
+            downControl.push(texto2[j]);
         }
-            for (let j = startReverse; j>=0; j--){
-                downControl.push(texto2[j]);
-            }
-    
-        saida1 = upControl.join("");
-        saida2 = downControl.join("");
-    
-        saida3= saida1.replace(/\s/g, "");
-        saida4= saida2.replace(/\s/g, "");
-    
-        console.log(saida3);
-        console.log(saida4);    
-    
-        if(saida3 == saida4){
-            return `true`;
-        }
-        return `false`; 
+
+    saida1 = upControl.join("");
+    saida2 = downControl.join("");
+
+    saida3= saida1.replace(/\s/g, "");
+    saida4= saida2.replace(/\s/g, "");
+
+    console.log(saida3);
+    console.log(saida4);    
+
+    if(saida3 == saida4){
+        return `true`;
+    }
+    return `false`; 
+
 }
 
 
